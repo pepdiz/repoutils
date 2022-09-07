7edd0ccd3696e9c776ed05688b19d44caa8d7ef1
pepdiz <socialkuma@hotmail.com>
2022-09-07 15:59:55 +0200

    renamed test suite

--- tests/test_repo-gen_param.sh
+++ tests/test_repo-gen_param.sh
@@ -1,3 +1,11 @@
+setup () {
+  export REPOFILE=.repo
+  export DIR=$(mktemp -d)
+}
+
+teardown () {
+  [ -d $DIR ] && rm -rf $DIR
+}
 
 test_h_or_help_show_syntax () {
   OUT='repo-gen [dir] [repo-name]
@@ -19,27 +27,31 @@ todo_other_flags_exit_with_error () {
 }
 
 test_dir_passed_is_invalid_dir () {
-  DIR=./non_existent_dir
-  assert_equals "$DIR is not a directory" "$(../repo-gen $DIR)"
+  NODIR=./non_existent_dir
+  assert_equals "$NODIR is not a directory" "$(../repo-gen $NODIR)"
 }
 
-test_when_invalid_dir_should_fail_with_code_1 () {
-  DIR=./non_existent_dir
-  assert_status_code 1 "../repo-gen $DIR"
+test_invalid_dir_should_fail_with_code_1 () {
+  NODIR=./non_existent_dir
+  assert_status_code 1 "../repo-gen $NODIR"
+}
+
+test_non_passing_dir_uses_current_dir () {
+  CURDIR=$(pwd)
+  cd $DIR
+  $CURDIR/../repo-gen
+  cd $PWD
+  assert_status_code 0 "test -f $DIR/$REPOFILE"  
 }
 
 test_non_passing_repo_name_makes_it_dir_name () {
-  DIR=$(mktemp -d)
   DIRNAME=$(basename $DIR)
   ../repo-gen $DIR
   assert_matches ".*$DIRNAME Porteus repository.*" "$(cat $DIR/index.html)"
-  rm -rf $DIR
 }
 
 test_passing_repo_name_uses_it () {
-  DIR=$(mktemp -d)
   REPONAME=mi_repo
   ../repo-gen $DIR $REPONAME
   assert_matches ".*$REPONAME Porteus repository.*" "$(cat $DIR/index.html)"
-  rm -rf $DIR
 }
